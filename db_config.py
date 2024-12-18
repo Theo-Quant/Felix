be3a73cbae72f7df89a1e06190ae6762f9dfb19b
felixJeong-quantdev <felix.jeong@theopartners.co>
2024-12-18 16:20:18 +0800

    upload the config.py for TrendUploadRedis

---
+++ db_config.py
@@ -0,0 +1,9 @@
+connection_string  = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=theosql.database.windows.net;"
+    "DATABASE=arbitrage_db_2024-03-22T23-30Z;"
+    "UID=THEOsql;"
+    "PWD=THEOBullRun2024!"
+)
+
+connection_string_dash = 'mssql+pyodbc://THEOsql:THEOBullRun2024!@theosql.database.windows.net/arbitrage_db_2024-03-22T23-30Z?driver=ODBC+Driver+17+for+SQL+Server'
